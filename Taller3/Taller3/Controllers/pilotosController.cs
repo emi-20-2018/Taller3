08c58ddc964429ee7e5cdd6c67ffc6b3d8019b1f
Emily <emi.roca18@gmail.com>
2021-02-09 12:12:58 -0400

    Creando pilotosController

---
+++ Taller3/Taller3/Controllers/pilotosController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Taller3.Models;
+
+namespace Taller3.Controllers
+{
+    public class pilotosController : ApiController
+    {
+        Taller3Entities dbContext = new Taller3Entities();
+        // GET api/pilotos
+        public IQueryable<pilotos> Get()
+        {
+            //pilotos lista= pilotos.
+            
+            var lista = dbContext.pilotos;
+           
+
+            return lista;
+        }
+
+        // GET api/pilotos/5
+        [ResponseType(typeof(pilotos))]
+        public IHttpActionResult Get(int id)
+        {   
+            pilotos buscado= dbContext.pilotos.Find(id);
+            if (buscado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(buscado);
+        }
+
+        // POST: api/pilotos
+        [ResponseType(typeof(pilotos))]
+        public IHttpActionResult Postpilotos(pilotos piloto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            dbContext.pilotos.Add(piloto);
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (dbContext.pilotos.Contains(piloto))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = piloto.codigo }, piloto);
+        }
+
+        // PUT: api/piloto/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putpiloto(int id, pilotos piloto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != piloto.codigo)
+            {
+                return BadRequest();
+            }
+
+            dbContext.Entry(piloto).State = EntityState.Modified;
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (dbContext.pilotos.Contains(piloto))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/piloto/5
+        [ResponseType(typeof(pilotos))]
+        public IHttpActionResult Deletepiloto(int id)
+        {
+            pilotos piloto = dbContext.pilotos.Find(id);
+            if (piloto == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.pilotos.Remove(piloto);
+            dbContext.SaveChanges();
+
+            return Ok(piloto);
+        }
+
+
+
+    }
+}
